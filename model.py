88f98c93a3f30a18839ef2885b122d29f5049da5
ramosv <56368263+ramosv@users.noreply.github.com>
2023-10-25 19:43:44 -0600

    Implemeting a Convolutional Neural Network (CNN) to compare against RandomForest Model. Although both are unconventional approaches to solve sudoku puzzles, it is still amazing that they can even solve one at all

--- model.py
+++ model.py
@@ -1,6 +1,13 @@
-from preprocess_data import preprocess_data
+from preprocess_data import preprocess_data, preprocess_data_2D
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
+
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten
+from keras.utils import to_categorical
+
 
 # Use the file path to your CSV file
 file_path = 'C:/Users/ramosv/Desktop/Github/Sudoku-Game/sudoku.csv'
@@ -14,6 +21,49 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
 accuracy=accuracy_score(y_test,y_pred)
-print(f"Model accuracy: {accuracy}")
-print("Classification report:")
+print(f"RandomForestClassifier Model accuracy: {accuracy}")
+print("RandomForestClassifier Classification report:")
 print(classification_report(y_test,y_pred))
+
+
+### CNN
+
+X_train, X_test, y_train, y_test = preprocess_data_2D(file_path)
+
+print("Shape of y_train:", np.array(y_train).shape)
+print("Shape of y_test:", np.array(y_test).shape)
+
+X_train = np.array(X_train).reshape(-1, 9, 9, 1)
+X_test = np.array(X_test).reshape(-1, 9, 9, 1)
+
+#y_train = to_categorical(y_train)
+#y_test = to_categorical(y_test)
+model = Sequential()
+model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(9, 9, 1)))
+model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+#model.summary()
+model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test), verbose=0)
+#model.summary()
+
+# Evaluate the model
+loss, accuracy = model.evaluate(X_test, y_test)
+print(f"Model accuracy: {accuracy}")
+
+# Predict the Probabilities
+y_pred_prob = model.predict(X_test)
+
+# Convert Probabilities to Class Labels
+y_pred_labels = np.argmax(y_pred_prob, axis=1)
+
+# Convert One-Hot Encoded Ground Truth to Class Labels
+y_true_labels = np.argmax(y_test, axis=1)
+
+# Generate and Print Classification Report
+print("Classification report:")
+print(classification_report(y_true_labels, y_pred_labels))
--- preprocess_data.py
+++ preprocess_data.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
 
+##### Preprocess data to be used in a RandomForestClassifier Model #####
 def load_data(path):
     #Load the data, only grabbing the first 10000
-    data = pd.read_csv(path,nrows=10000)
+    data = pd.read_csv(path,nrows=2000)
     return data
 
 #Preprocess data as grid
@@ -36,7 +39,44 @@ def preprocess_sudoku(s):
 
     return sudoku
 
-   
+#### Pre-processing Data to be represented as a 2D grid to be used in a Convolutional Neural Network(CNN)
+
+def preprocess_sudoku2D(s):
+    #Conver things into 2D arrays of strings
+    return [list(map(int, list(s[i:i+9]))) for i in range(0, len(s), 9)]
+def process_solutions_2D(data):
+    puzzles = data['puzzle'].tolist()
+    solutions = data['solution'].tolist()
+
+    X, y = [], []
+
+    for p, s in zip(puzzles, solutions):
+        puzzle_grid = preprocess_sudoku2D(p)
+        solution_grid = preprocess_sudoku2D(s)
+
+        # Loop over each cell in the sudoku grid
+        for i in range(9):
+            for j in range(9):
+                # Make a deep copy of the puzzle grid
+                modified_puzzle = [row.copy() for row in puzzle_grid]
+                # Blank out the cell we are trying to predict
+                modified_puzzle[i][j] = 0
+                
+                X.append(modified_puzzle)
+
+                #print("Value to be encoded:", solution_grid[i][j])
+
+                # We use to_categorical to convert the number into one-hot encoding
+                encoded_cell = to_categorical(solution_grid[i][j], num_classes=10)
+                #print("Shape of encoded cell:", encoded_cell.shape)
+                y.append(encoded_cell)
+
+    y = np.array(y)
+    #print("Shape of y:", y.shape)
+
+    return X, y
+
+
 def process_solutions(data):
     puzzles = data['puzzle'].tolist()
     solutions = data['solution'].tolist()
@@ -57,14 +97,31 @@ def process_solutions(data):
 def training_testing_data(puzzles, solutions,testSize =0.2):
     #Splitting data into training and training sets
     X_train, X_test, y_train, y_test = train_test_split(puzzles, solutions, test_size=testSize, random_state=42)
+    # print("Shape of X_train:", np.array(X_train).shape)
+    # print("Shape of X_test:", np.array(X_test).shape)
+    # print("Shape of y_train:", np.array(y_train).shape)
+    # print("Shape of y_test:", np.array(y_test).shape)
+
     return X_train, X_test, y_train, y_test
 
+
+#### RandomForestClassifier Model #####
 def preprocess_data(path):
     data = load_data(path)
     puzzles, solutions = process_solutions(data)
 
     #Training and testing data for model
     return training_testing_data(puzzles,solutions)
+
+#### Convolutional Neural Network(CNN)
+def preprocess_data_2D(path):
+    data = load_data(path)
+    puzzles, solutions = process_solutions_2D(data)
+
+    # Training and testing data for model
+    return training_testing_data(puzzles, solutions)
+
+
 
 
 
